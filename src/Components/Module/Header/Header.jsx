a06641be2cd2f83bbb3c2436601652f3379cb8ec
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-02-04 16:46:01 +0330

    create section header

--- src/Components/Module/Header/Header.jsx
+++ src/Components/Module/Header/Header.jsx
@@ -1,10 +1,34 @@
+import { Link, NavLink } from 'react-router-dom'
 import './Header.css'
+import { FaInstagram, FaGithub, FaTelegram   } from "react-icons/fa";
+
 
 function Header() {
   return (
     <section className="header bg_back">
         <div className="wrapper_header">
-            this is header
+            <div className="header_title_textJob">
+                <h1 className="header_title">محمدمهدی دالوندی</h1>
+                <span className="header_textJob">برنامه نویس وب</span>
+            </div>
+            <div className="header_media">
+                <NavLink to='' className="header_media_link">
+                    <span className="header_media_icon link_icon_insta">
+                        <FaInstagram/>
+                    </span>
+                </NavLink>
+                <NavLink to='' className="header_media_link">
+                    <span className="header_media_icon link_icon_git">
+                        <FaGithub />
+                    </span>
+                </NavLink>
+                <NavLink to='' className="header_media_link">
+                    <span className="header_media_icon link_icon_tel">
+                        <FaTelegram />
+                    </span>
+                </NavLink>
+            </div>
+            <Link src="" className='header_resume' target='_blank' download>دانلود رزومه</Link>
         </div>
     </section>
   )
