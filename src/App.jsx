b5a457bf9c49fddab5883633d992d4cd2a3edbc8
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-02-04 15:52:19 +0330

    create section sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useRoutes } from "react-router-dom"
 import routes from "../routes"
 
+import Sidebar from './Components/Module/Sidebar/Sidebar'
+
 function App() {
   const router = useRoutes(routes)
 
   return (
     <div className="container">
         <div className="row mt-5 mb-5">
-          <div className="col-lg-2">
-            sidebar
+          <div className="col-lg-3">
+            <Sidebar/>
           </div>
-          <div className="col-lg-10">
+          <div className="col-lg-9">
             <div className="row">
               <div className="col-12">navbar</div>
               <div className="col-12 mt-5">
---
+++ src/Components/Module/Sidebar/Sidebar.jsx
@@ -0,0 +1,81 @@
+import { NavLink, useLocation } from 'react-router-dom'
+import './Sidebar.css'
+import { IoPerson, IoDocumentText  } from "react-icons/io5";
+import { MdOutlineMiscellaneousServices, MdWorkspaces, MdArticle   } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { useState } from 'react';
+
+
+
+function Sidebar() {
+    const [activeMenu , setActiveMenu ] = useState('/')
+    const location = useLocation()
+
+    useLocation(()=>{
+        const pathName = location.pathname
+        setActiveMenu(pathName)
+    },[location])
+  return (
+    <section className="sidebar">
+        <div className="wrapper_sidebar">
+            <ul className="sidebar_items">
+            <li className="sidebar_item">
+                    <NavLink to=''className='sidebar_item_link-img' >
+                        <img src="/assets/images/mhd.png" alt="" className="sidebar_item_img" />
+                    </NavLink>
+                </li>
+                <li className="sidebar_item">
+                    <NavLink to='/'className={`sidebar_item_link ${activeMenu === '/' ? "sidebar_item_linkActive" :""}`} >
+                        <span className="sidebar_item_icon">
+                            <IoPerson/>
+                        </span>
+                        <span className="sidebar_item_text">درباره من</span>
+                    </NavLink>
+                </li>
+                <li className="sidebar_item">
+                    <NavLink to='/r'className={`sidebar_item_link ${activeMenu === '/r' ? "sidebar_item_linkActive" :""}`} >
+                        <span className="sidebar_item_icon">
+                            <IoDocumentText />
+                        </span>
+                        <span className="sidebar_item_text">رزومه من</span>
+                    </NavLink>
+                </li>
+                <li className="sidebar_item">
+                    <NavLink to='/Services'className={`sidebar_item_link ${activeMenu === '/Services' ? "sidebar_item_linkActive" :""}`} >
+                        <span className="sidebar_item_icon">
+                            <MdOutlineMiscellaneousServices/>
+                        </span>
+                        <span className="sidebar_item_text">خدمات</span>
+                    </NavLink>
+                </li>
+                <li className="sidebar_item">
+                    <NavLink to='/SampleWork'className={`sidebar_item_link ${activeMenu === '/SampleWork' ? "sidebar_item_linkActive" :""}`} >
+                        <span className="sidebar_item_icon">
+                            <MdWorkspaces />
+                        </span>
+                        <span className="sidebar_item_text">نمونه کارها</span>
+                    </NavLink>
+                </li>
+                <li className="sidebar_item">
+                    <NavLink to='/Article'className={`sidebar_item_link ${activeMenu === '/Article' ? "sidebar_item_linkActive" :""}`} >
+                        <span className="sidebar_item_icon">
+                            <MdArticle />
+                        </span>
+                        <span className="sidebar_item_text">وبلاگ</span>
+                    </NavLink>
+                </li>
+                <li className="sidebar_item">
+                    <NavLink to='/Contact'className={`sidebar_item_link ${activeMenu === '/Contact' ? "sidebar_item_linkActive" :""}`} >
+                        <span className="sidebar_item_icon">
+                            <FaPhoneAlt/>
+                        </span>
+                        <span className="sidebar_item_text">تماس با من</span>
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
+    </section>
+  )
+}
+
+export default Sidebar
