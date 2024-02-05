198fc00928390c7b9e8b17077967040d4d8d7d80
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-02-05 15:51:48 +0330

    create sampleCard

---
+++ src/Components/Module/SampleCard/SampleCard.jsx
@@ -0,0 +1,15 @@
+import { NavLink } from 'react-router-dom'
+import './SampleCard.css'
+
+function SampleCard() {
+  return (
+    <div className="col-lg-4 col-md-6 col-sm-12">
+    <NavLink className="sampleCard">
+        <img src="/assets/images/pro.jpg" alt="" className="sampleCard_img" />
+        <span className="sampleCard_name">سایت فروشگاهی</span>
+    </NavLink>
+</div>
+  )
+}
+
+export default SampleCard
